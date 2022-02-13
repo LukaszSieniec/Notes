a16d3c61d9b307ba9aaaae91d6fd65479516dd3e
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2022-02-14 00:04:23 +0100

    Refactor Note model

--- notes_app/lib/models/note.dart
+++ notes_app/lib/models/note.dart
@@ -1,12 +1,17 @@
-class Note {
+import 'package:equatable/equatable.dart';
+
+class Note extends Equatable {
   final String content;
   final int creationDate;
 
-  Note({required this.content, required this.creationDate});
+  const Note({required this.content, required this.creationDate});
 
   factory Note.fromJson(Map<String, dynamic> json) =>
       Note(content: json['content'], creationDate: json['creation_date']);
 
   Map<String, dynamic> toJson() =>
       {'content': content, 'creation_date': creationDate};
+
+  @override
+  List<Object> get props => [content, creationDate];
 }
