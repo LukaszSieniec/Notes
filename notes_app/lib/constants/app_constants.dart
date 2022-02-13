24d75e5152c138d6baca621e9426d6dd5b2c78e9
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2022-02-13 16:02:13 +0100

    Add constants

---
+++ notes_app/lib/constants/app_constants.dart
@@ -0,0 +1,12 @@
+class AppColor {
+  static const int background = 0xFF2E2E47;
+  static const int brighterBackground = 0xFF34344f;
+  static const int colorText = 0xFFA4A4b1;
+}
+
+class AppLabels {
+  static const String notes = 'Notes';
+  static const String addNote = 'Add Note';
+}
+
+class AppMessages {}
