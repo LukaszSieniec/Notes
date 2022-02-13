ea3a93014404ccc2d286f3bbd4a88491db734580
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2022-02-13 21:53:12 +0100

    Add MultiBlocProvider and BlocBuilder in HomeScreen

--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/bloc/add_note/add_note_bloc.dart';
+import 'package:notes_app/repositories/notes_repository.dart';
 import 'package:notes_app/views/screens/home_screen.dart';
+
+import 'bloc/notes/notes_bloc.dart';
+import 'bloc/notes/notes_event.dart';
+import 'database/notes_database.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +18,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: HomeScreen());
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (context) =>
+                  AddNoteBloc(NotesRepository(NotesDatabase.get))),
+          BlocProvider(
+              create: (context) => NotesBloc(NotesRepository(NotesDatabase.get))
+                ..add(const NotesLoaded()))
+        ],
+        child: const MaterialApp(
+            debugShowCheckedModeBanner: false, home: HomeScreen()));
   }
 }
--- notes_app/lib/views/screens/home_screen.dart
+++ notes_app/lib/views/screens/home_screen.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/bloc/notes/notes_bloc.dart';
+import 'package:notes_app/bloc/notes/notes_state.dart';
 import 'package:notes_app/constants/app_constants.dart';
 import 'package:notes_app/views/screens/add_note_screen.dart';
-import 'package:notes_app/views/widgets/notes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color(AppColors.background),
-        appBar: AppBar(
-            title:
-                const Text(AppLabels.notes, style: TextStyle(fontSize: 24.0)),
-            centerTitle: true,
-            backgroundColor: Colors.transparent,
-            elevation: 0),
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.green,
-            onPressed: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const AddNoteScreen())),
-            child: const Icon(Icons.add)),
-        body: Column(
-            children: const [Divider(height: 2.0, color: Colors.white)]));
+    return BlocBuilder<NotesBloc, NotesState>(builder: (context, state) {
+      return Scaffold(
+          backgroundColor: const Color(AppColors.background),
+          appBar: AppBar(
+              title:
+                  const Text(AppLabels.notes, style: TextStyle(fontSize: 24.0)),
+              centerTitle: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0),
+          floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.green,
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AddNoteScreen())),
+              child: const Icon(Icons.add)),
+          body: Column(
+              children: const [Divider(height: 2.0, color: Colors.white)]));
+    });
   }
 }
