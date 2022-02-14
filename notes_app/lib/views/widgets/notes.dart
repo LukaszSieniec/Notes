a6e6953a8a5785a0545ade5f1627908d06cacf71
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2022-02-14 23:23:56 +0100

    Refactor notes

--- notes_app/lib/views/widgets/notes.dart
+++ notes_app/lib/views/widgets/notes.dart
@@ -12,16 +12,22 @@ class Notes extends StatelessWidget {
   Widget build(BuildContext context) {
     SizeConfig.init(context);
     return Expanded(
-        child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: notes.length,
-            itemBuilder: (context, index) {
-              return NoteItem(
-                  note: notes[index],
-                  horizontalPadding: SizeConfig.blockSizeHorizontal * 4.45,
-                  verticalPadding: SizeConfig.blockSizeVertical * 1.15,
-                  contentSize: SizeConfig.blockSizeVertical * 3.35,
-                  creationDateSize: SizeConfig.blockSizeVertical * 2.25);
-            }));
+        child: Container(
+            margin: EdgeInsets.only(
+                left: SizeConfig.blockSizeHorizontal * 2.25,
+                top: SizeConfig.blockSizeVertical * 1.15,
+                right: SizeConfig.blockSizeHorizontal * 2.25,
+                bottom: SizeConfig.blockSizeVertical * 1.15),
+            child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: notes.length,
+                itemBuilder: (context, index) {
+                  return NoteItem(
+                      note: notes[index],
+                      horizontalPadding: SizeConfig.blockSizeHorizontal * 4.45,
+                      verticalPadding: SizeConfig.blockSizeVertical * 1.15,
+                      contentSize: SizeConfig.blockSizeVertical * 3.35,
+                      creationDateSize: SizeConfig.blockSizeVertical * 2.25);
+                })));
   }
 }
