a84ea94f3367ddea119849528d114ebe25f0b16d
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2022-02-15 21:32:21 +0100

    Refactor HomeScreen

--- notes_app/lib/views/screens/home_screen.dart
+++ notes_app/lib/views/screens/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:notes_app/bloc/notes/notes_state.dart';
 import 'package:notes_app/constants/app_constants.dart';
 import 'package:notes_app/utils/size_config.dart';
 import 'package:notes_app/views/screens/add_note_screen.dart';
+import 'package:notes_app/views/widgets/loading.dart';
 import 'package:notes_app/views/widgets/notes.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,28 +15,41 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     SizeConfig.init(context);
     return BlocBuilder<NotesBloc, NotesState>(builder: (context, state) {
-      return Scaffold(
-          backgroundColor: const Color(AppColors.background),
-          appBar: AppBar(
-              title: Text(AppLabels.notes,
-                  style:
-                      TextStyle(fontSize: SizeConfig.blockSizeVertical * 3.35)),
-              centerTitle: true,
-              flexibleSpace: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Divider(
-                      height: SizeConfig.blockSizeVertical * 0.30,
-                      color: Colors.white)),
-              backgroundColor: Colors.transparent,
-              elevation: 0),
-          floatingActionButton: FloatingActionButton(
-              backgroundColor: Colors.green,
-              onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const AddNoteScreen())),
-              child: const Icon(Icons.add)),
-          body: Notes(notes: state.notes));
+      if (state.notesStatus == NotesStatus.loading) {
+        return _buildScaffold(buildLoading(), context, isVisible: false);
+      } else if (state.notesStatus == NotesStatus.success) {
+        return _buildScaffold(Notes(notes: state.notes), context);
+      } else {
+        return _buildScaffold(Container(), context);
+      }
     });
+  }
+
+  Widget _buildScaffold(Widget body, BuildContext context,
+      {bool isVisible = true}) {
+    return Scaffold(
+        backgroundColor: const Color(AppColors.background),
+        appBar: AppBar(
+            title: Text(AppLabels.notes,
+                style:
+                    TextStyle(fontSize: SizeConfig.blockSizeVertical * 3.35)),
+            centerTitle: true,
+            flexibleSpace: Align(
+                alignment: Alignment.bottomCenter,
+                child: Divider(
+                    height: SizeConfig.blockSizeVertical * 0.30,
+                    color: Colors.white)),
+            backgroundColor: Colors.transparent,
+            elevation: 0),
+        floatingActionButton: isVisible
+            ? FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const AddNoteScreen())),
+                child: const Icon(Icons.add))
+            : Container(),
+        body: body);
   }
 }
