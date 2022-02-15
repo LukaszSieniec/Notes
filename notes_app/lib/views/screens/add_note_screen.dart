e11b918d09518763654672424ec21568a8a25f9e
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2022-02-15 22:13:07 +0100

    Refactor AddNoteScreen

--- notes_app/lib/views/screens/add_note_screen.dart
+++ notes_app/lib/views/screens/add_note_screen.dart
@@ -5,6 +5,7 @@ import 'package:notes_app/bloc/add_note/add_note_state.dart';
 import 'package:notes_app/constants/app_constants.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/utils/size_config.dart';
+import 'package:notes_app/views/widgets/loading.dart';
 import 'package:notes_app/views/widgets/snackbar.dart';
 
 class AddNoteScreen extends StatefulWidget {
@@ -31,47 +32,11 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
             .showSnackBar(PrimarySnackBar(title: AppMessages.error));
       }
     }, builder: (context, state) {
-      return Scaffold(
-          backgroundColor: const Color(AppColors.background),
-          appBar: AppBar(
-              title: Text(AppLabels.addNote,
-                  style:
-                      TextStyle(fontSize: SizeConfig.blockSizeVertical * 3.35)),
-              centerTitle: true,
-              flexibleSpace: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Divider(
-                      height: SizeConfig.blockSizeVertical * 0.30,
-                      color: Colors.white)),
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              automaticallyImplyLeading: false,
-              actions: [
-                IconButton(
-                    onPressed: () => context
-                        .read<AddNoteBloc>()
-                        .add(SavedNote(_contentController.text)),
-                    icon: Icon(Icons.save,
-                        size: SizeConfig.blockSizeVertical * 4.0))
-              ]),
-          body: Container(
-              margin: EdgeInsets.only(
-                  left: SizeConfig.blockSizeHorizontal * 8.90,
-                  top: SizeConfig.blockSizeVertical * 4.45,
-                  right: SizeConfig.blockSizeHorizontal * 8.90),
-              padding: EdgeInsets.only(
-                  left: SizeConfig.blockSizeHorizontal * 4.45,
-                  right: SizeConfig.blockSizeHorizontal * 4.45),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                      Radius.circular(SizeConfig.blockSizeVertical * 2.25)),
-                  color: Colors.white),
-              child: TextFormField(
-                  controller: _contentController,
-                  minLines: 3,
-                  maxLines: null,
-                  decoration:
-                      const InputDecoration(border: InputBorder.none))));
+      if (state.addNoteStatus == AddNoteStatus.loading) {
+        return _buildScaffold(buildLoading(), isVisible: false);
+      } else {
+        return _buildScaffold(_buildAddNoteScreen());
+      }
     });
   }
 
@@ -79,5 +44,53 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
   void dispose() {
     _contentController.dispose();
     super.dispose();
+  }
+
+  Widget _buildScaffold(Widget body, {bool isVisible = true}) {
+    return Scaffold(
+        backgroundColor: const Color(AppColors.background),
+        appBar: AppBar(
+            title: Text(AppLabels.addNote,
+                style:
+                    TextStyle(fontSize: SizeConfig.blockSizeVertical * 3.35)),
+            centerTitle: true,
+            flexibleSpace: Align(
+                alignment: Alignment.bottomCenter,
+                child: Divider(
+                    height: SizeConfig.blockSizeVertical * 0.30,
+                    color: Colors.white)),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            automaticallyImplyLeading: false,
+            actions: [
+              if (isVisible)
+                IconButton(
+                    onPressed: () => context
+                        .read<AddNoteBloc>()
+                        .add(SavedNote(_contentController.text)),
+                    icon: Icon(Icons.save,
+                        size: SizeConfig.blockSizeVertical * 4.0))
+            ]),
+        body: body);
+  }
+
+  Widget _buildAddNoteScreen() {
+    return Container(
+        margin: EdgeInsets.only(
+            left: SizeConfig.blockSizeHorizontal * 8.90,
+            top: SizeConfig.blockSizeVertical * 4.45,
+            right: SizeConfig.blockSizeHorizontal * 8.90),
+        padding: EdgeInsets.only(
+            left: SizeConfig.blockSizeHorizontal * 4.45,
+            right: SizeConfig.blockSizeHorizontal * 4.45),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+                Radius.circular(SizeConfig.blockSizeVertical * 2.25)),
+            color: Colors.white),
+        child: TextFormField(
+            controller: _contentController,
+            minLines: 3,
+            maxLines: null,
+            decoration: const InputDecoration(border: InputBorder.none)));
   }
 }
